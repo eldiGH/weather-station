97d257cfd38f718c8d8fb9ad49b873b2e0de5543
Dawid Marinoni <saurixyz2@gmail.com>
2023-08-31 11:59:45 +0200

    Remove unnecessary https server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,8 @@
 import 'dotenv/config';
 import { app } from './app';
 import { testDBConnection } from './db';
-import https from 'https';
-import http from 'http';
-import { readFile } from 'fs/promises';
-import { join } from 'path';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-const certLocation = process.env.CERT_LOCATION ?? join(__dirname, '..');
-const useHttp = process.env.HTTP === 'true';
 
 const main = async () => {
   const dbError = await testDBConnection();
@@ -19,22 +13,9 @@ const main = async () => {
     return;
   }
 
-  try {
-    const cert = await readFile(join(certLocation, 'cert.pem'), { encoding: 'utf8' });
-    const key = await readFile(join(certLocation, 'key.pem'), { encoding: 'utf8' });
-
-    https.createServer({ cert, key }, app).listen(port, () => {
-      console.log(`HTTPS Server running on port ${port}`);
-    });
-
-    if (useHttp) {
-      http.createServer(app).listen(port + 1, () => {
-        console.log(`HTTP Server running on port ${port + 1}`);
-      });
-    }
-  } catch (e) {
-    console.error(e);
-  }
+  app.listen(port, () => {
+    console.log(`HTTP Server running on port ${port}`);
+  });
 };
 
 main();
