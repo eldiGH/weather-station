af1d64d973706fbf4a3717f2aaf69ae5f61fba66
Dawid Marinoni <saurixyz2@gmail.com>
2024-01-15 17:39:38 +0100

    packages cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,4 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 await connectToRedis();
 
-server.listen({ port, host: '0.0.0.0' });
+server.listen({ port, host: 'localhost' });
--- frontend/src/routes/(authorized)/+page.svelte
+++ frontend/src/routes/(authorized)/+page.svelte
@@ -1,26 +1,14 @@
 <script lang="ts">
-	import LineChart from '$lib/components/LineChart.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
+	import Button from '$lib/components/Button.svelte';
 </script>
 
 <div class="root">
-	{#if data.lastSensorEntry}
-		<div>
-			<div>Temperatura: {data.lastSensorEntry.temperature} °C</div>
-			<div>Wilgotność: {data.lastSensorEntry.humidity}%</div>
-			<div>Ciśnienie: {data.lastSensorEntry.pressure} hPa</div>
-			<div>Oporność gazów: {data.lastSensorEntry.gasResistance} Ohms</div>
-			<div>Ostatnie dane z: {data.lastSensorEntry.createdAt}</div>
-		</div>
-	{/if}
-
-	<LineChart config={data.config} />
+	<Button>Test</Button>
 </div>
 
 <style lang="scss">
 	.root {
 		width: 100%;
+		padding: 20rem;
 	}
 </style>
