cccd57816b9ee0ab792e3dc8eec172aeaf6c243e
Dawid Marinoni <saurixyz2@gmail.com>
2024-10-09 12:55:04 +0200

    Fix typescript

--- frontends/weather-station/vite.config.ts
+++ frontends/weather-station/vite.config.ts
@@ -1,30 +1,3 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import config from 'frontend.shared/config.json' with { type: 'json' };
+import viteConfig from 'frontend.shared/vite.config.js';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		proxy: {
-			'^/trpc/*': {
-				target: config.serverAddress,
-				rewrite: (path) => path.replace('/trpc', '')
-			},
-			'^/trpcws/*': {
-				target: config.serverAddress.replace('http', 'ws'),
-				rewrite: (path) => path.replace('/trpcws', ''),
-				ws: true
-			}
-		}
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: 'modern-compiler'
-			}
-		}
-	}
-});
+export default viteConfig;
